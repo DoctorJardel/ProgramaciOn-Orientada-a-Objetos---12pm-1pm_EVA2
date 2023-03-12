20af815c584a2741a52147f503cb113fdb65334f
DoctorJardel <124392067+DoctorJardel@users.noreply.github.com>
2023-03-12 16:50:19 -0600

    Add files via upload

---
+++ EVA2_7_This/src/eva2_7_this/EVA2_7_This.java
@@ -0,0 +1,40 @@
+
+
+package eva2_7_this;
+
+
+public class EVA2_7_This {
+
+    
+    public static void main(String[] args) {
+       
+    }
+    
+}
+class persona{
+    private String nombre;
+    private String apellido;
+
+    public persona(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String valor) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    
+        
+}
