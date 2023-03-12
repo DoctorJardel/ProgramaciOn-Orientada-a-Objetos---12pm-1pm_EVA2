5e380b62a417799e4f212d8f65e708dfda41e1c8
DoctorJardel <124392067+DoctorJardel@users.noreply.github.com>
2023-03-12 12:34:58 -0600

    Add files via upload

---
+++ EVA2_2_HERENCIA/src/eva2_2_herencia/ESTUDIANTE.java
@@ -0,0 +1,41 @@
+
+public class ESTUDIANTE {
+    private String nombre;
+    private String apellido;
+    private int edad;
+    public String nocontrol;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNocontrol() {
+        return nocontrol;
+    }
+
+    public void setNocontrol(String nocontrol) {
+        this.nocontrol = nocontrol;
+    }
+    
+    
+}
