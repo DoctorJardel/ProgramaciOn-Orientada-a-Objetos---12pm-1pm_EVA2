50a2712e57066df26e59fc1737d2d80cd951f8c9
DoctorJardel <124392067+DoctorJardel@users.noreply.github.com>
2023-03-12 13:13:42 -0600

    Add files via upload

---
+++ EVA2_5_Herencia_/src/eva2_5_herencia_/EVA2_5_Herencia_.java
@@ -0,0 +1,20 @@
+package eva2_5_herencia_;
+public class EVA2_5_Herencia_ {
+
+    public static void main(String[] args) {
+        
+     SubClase = object new SubClase();
+    }
+}
+class SuperClase{
+    public void mensaje(){
+        System.out.println("Hola");
+    }
+    
+}
+
+class SubClase extends SuperClase{
+    
+    
+}
+
